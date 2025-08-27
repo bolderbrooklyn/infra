d4eff7d175cc6f98d28ad62bb9f5ca810187b038
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-08-26 17:22:03 -0700

    system activation must now be run as root

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install
 
 # install nix-darwin and apply the flake
-nix run nix-darwin/master#darwin-rebuild -- switch --flake .$(hostname -s)
+sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#"$(hostname -s)"
