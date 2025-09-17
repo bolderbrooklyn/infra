f06e37ce0de8b94e1364412f091f09e5327f3a4e
jesse brooklyn hannah <jesse@jbhannah.net>
2025-09-17 16:16:09 -0700

    fix ssh enableDefaultConfig warning

--- nix/platforms/common/home.nix
+++ nix/platforms/common/home.nix
@@ -8,81 +8,88 @@
   home-manager.useGlobalPkgs = true;
   home-manager.backupFileExtension = "backup";
 
-  home-manager.users.${config.common.username} = {
-    home.stateVersion = "25.05";
+  home-manager.users.${config.common.username} =
+    { config, ... }:
+    {
+      home.stateVersion = "25.05";
 
-    home.packages = with pkgs; [
-      httpie
-    ];
-
-    programs.btop = {
-      enable = true;
-
-      settings = {
-        vim_keys = true;
-      };
-    };
-
-    programs.fd = {
-      enable = true;
-      hidden = true;
-
-      ignores = [
-        "*.app/"
-        "*.photoslibrary/"
-        ".Trash/"
-        ".aitk/"
-        ".azure/"
-        ".cache/"
-        ".cargo/"
-        ".colima/_lima/"
-        ".direnv/"
-        ".docker/"
-        ".gem/"
-        ".git/"
-        ".jj/"
-        ".lmstudio/"
-        ".local/share"
-        ".local/state"
-        ".mono/"
-        ".npm/"
-        ".pytest_cache/"
-        ".rustup/"
-        ".stfolder/"
-        ".venv/"
-        ".vscode/extensions/"
-        ".yarn/"
-        "Library/"
-        "__pycache__/"
-        "cache/"
-        "node_modules/"
-        "out/"
-        "refs/"
-        "tmp/"
+      home.packages = with pkgs; [
+        httpie
       ];
+
+      programs.btop = {
+        enable = true;
+
+        settings = {
+          vim_keys = true;
+        };
+      };
+
+      programs.fd = {
+        enable = true;
+        hidden = true;
+
+        ignores = [
+          "*.app/"
+          "*.photoslibrary/"
+          ".Trash/"
+          ".aitk/"
+          ".azure/"
+          ".cache/"
+          ".cargo/"
+          ".colima/_lima/"
+          ".direnv/"
+          ".docker/"
+          ".gem/"
+          ".git/"
+          ".jj/"
+          ".lmstudio/"
+          ".local/share"
+          ".local/state"
+          ".mono/"
+          ".npm/"
+          ".pytest_cache/"
+          ".rustup/"
+          ".stfolder/"
+          ".venv/"
+          ".vscode/extensions/"
+          ".yarn/"
+          "Library/"
+          "__pycache__/"
+          "cache/"
+          "node_modules/"
+          "out/"
+          "refs/"
+          "tmp/"
+        ];
+      };
+
+      programs.fzf = {
+        enable = true;
+
+        defaultCommand = "fd --type f --hidden";
+        changeDirWidgetCommand = "fd --type d --hidden";
+      };
+
+      programs.gpg.enable = true;
+      services.gpg-agent = {
+        enable = true;
+        pinentry.package = lib.mkIf pkgs.stdenv.isDarwin pkgs.pinentry_mac;
+      };
+
+      programs.home-manager.enable = true;
+
+      programs.ripgrep.enable = true;
+
+      programs.ssh = {
+        enable = true;
+      }
+      // lib.optionalAttrs (builtins.hasAttr "enableDefaultConfig" config.programs.ssh) {
+        enableDefaultConfig = false;
+      };
+
+      programs.zoxide.enable = true;
+
+      xdg.enable = true;
     };
-
-    programs.fzf = {
-      enable = true;
-
-      defaultCommand = "fd --type f --hidden";
-      changeDirWidgetCommand = "fd --type d --hidden";
-    };
-
-    programs.gpg.enable = true;
-    services.gpg-agent = {
-      enable = true;
-      pinentry.package = lib.mkIf pkgs.stdenv.isDarwin pkgs.pinentry_mac;
-    };
-
-    programs.home-manager.enable = true;
-
-    programs.ripgrep.enable = true;
-
-    programs.ssh.enable = true;
-
-    programs.zoxide.enable = true;
-
-    xdg.enable = true;
-  };
 }
