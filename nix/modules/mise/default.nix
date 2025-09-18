58fd3d5306c32a8c151323ee1ab9f841c6b75604
jesse brooklyn hannah <jesse@jbhannah.net>
2025-09-18 08:49:34 -0700

    disable idiomatic version files in mise by default

--- nix/modules/mise/default.nix
+++ nix/modules/mise/default.nix
@@ -2,7 +2,13 @@
 {
   config = {
     home-manager.users.${config.common.username} = {
-      programs.mise.enable = true;
+      programs.mise = {
+        enable = true;
+
+        globalConfig.settings = {
+          idiomatic_version_file_enable_tools = [ ];
+        };
+      };
     };
   };
 }
