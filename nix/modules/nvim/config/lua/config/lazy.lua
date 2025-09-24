6362a90d4fdb7b364e787d6fe72edc76313de803
jesse brooklyn hannah <jesse@jbhannah.net>
2025-09-24 16:16:17 -0700

    readd copilot

--- nix/modules/nvim/config/lua/config/lazy.lua
+++ nix/modules/nvim/config/lua/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
 			},
 		},
 
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
+		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.coding.neogen" },
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
