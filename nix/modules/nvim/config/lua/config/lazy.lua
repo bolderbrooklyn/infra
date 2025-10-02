25102206c735102d0d0c2ee1d2a3739b404b6775
jesse brooklyn hannah <jesse@jbhannah.net>
2025-10-02 10:26:34 -0700

    add biome to lazyvim

--- nix/modules/nvim/config/lua/config/lazy.lua
+++ nix/modules/nvim/config/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.inc-rename" },
 		{ import = "lazyvim.plugins.extras.editor.overseer" },
 		{ import = "lazyvim.plugins.extras.editor.refactoring" },
+		{ import = "lazyvim.plugins.extras.formatting.biome" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
--- nix/modules/nvim/config/lua/config/options.lua
+++ nix/modules/nvim/config/lua/config/options.lua
@@ -1,3 +1,5 @@
+vim.g.lazyvim_prettier_needs_config = true
+
 vim.g.snacks_animate = false
 
 vim.opt.spellcapcheck = ""
