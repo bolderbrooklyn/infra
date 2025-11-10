7ea4cb37ac5a2c7e17ef49769fdd407cb4b52f93
jesse brooklyn hannah <jesse@jbhannah.net>
2025-11-09 17:24:06 -0800

    add tailscale up flags

--- nix/modules/tailscale/default.nix
+++ nix/modules/tailscale/default.nix
@@ -3,5 +3,9 @@
     enable = true;
     useRoutingFeatures = "both";
     openFirewall = true;
+    extraSetFlags = [
+      "--exit-node-allow-lan-access=true"
+      "--exit-node=us-sjc-wg-402.mullvad.ts.net."
+    ];
   };
 }
