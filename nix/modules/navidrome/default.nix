9cc8fc3e9887692d36a1ee3503c2db9857e8fd1c
jesse brooklyn hannah <jesse@jbhannah.net>
2026-01-05 07:34:28 -0800

    start navidrome after mounting media

--- nix/modules/navidrome/default.nix
+++ nix/modules/navidrome/default.nix
@@ -4,4 +4,7 @@
     group = "media";
     settings.MusicFolder = "/mnt/genesect/media/Music";
   };
+
+  systemd.services.navidrome.after = [ "mnt-genesect-media.mount" ];
+  systemd.services.navidrome.requires = [ "mnt-genesect-media.mount" ];
 }
