fa2a7f97743c368ad790228db2f1e8f72ddc1cb8
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-03 10:26:00 -0800

    update forgejo config

--- nix/modules/forgejo/default.nix
+++ nix/modules/forgejo/default.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 let
   forgejo = {
-    domain = "forgejo.anteater-wall.ts.net";
-    stateDir = "/mnt/genesect/forgejo";
+    domain = "git.anteater-wall.ts.net";
+    storageDir = "/mnt/genesect/forgejo";
     port = 3000;
   };
 in
@@ -10,9 +10,17 @@ in
   services.forgejo = {
     enable = true;
     package = pkgs.forgejo;
-    stateDir = forgejo.stateDir;
-    dump.enable = true;
-    lfs.enable = true;
+    repositoryRoot = "${forgejo.storageDir}/repositories";
+
+    dump = {
+      enable = true;
+      backupDir = "${forgejo.storageDir}/backups";
+    };
+
+    lfs = {
+      enable = true;
+      contentDir = "${forgejo.storageDir}/lfs";
+    };
 
     settings = {
       session.COOKIE_SECURE = true;
@@ -22,32 +30,37 @@ in
         HTTP_PORT = forgejo.port;
         ROOT_URL = "https://${forgejo.domain}/";
       };
+      service.DISABLE_REGISTRATION = true;
     };
   };
 
   services.gitea-actions-runner = {
-    instances = {
-      tinkaton = {
-        enable = true;
-        name = "tinkaton";
-        labels = [
-          "debian-latest:docker://node:lts-bullseye"
-          "ubuntu-latest:docker://node:lts-bullseye"
-        ];
-        url = "http://localhost:3000";
-        tokenFile = config.age.secrets."gitea-actions-runner-forgejo".path;
-      };
+    package = pkgs.forgejo-runner;
+
+    instances.tinkaton = {
+      enable = true;
+      name = "tinkaton";
+      labels = [
+        "debian-latest:docker://node:lts-bullseye"
+        "ubuntu-latest:docker://node:lts-bullseye"
+      ];
+      url = "http://localhost:3000";
+      tokenFile = config.age.secrets."gitea-actions-runner-forgejo".path;
     };
+
   };
 
   networking.firewall.allowedTCPPorts = [ forgejo.port ];
 
-  fileSystems."${forgejo.stateDir}" = {
+  fileSystems."${forgejo.storageDir}" = {
     device = "genesect.home.local:/nfs/Forgejo";
     fsType = "nfs";
-    options = [
-      "noatime"
-      "nodiratime"
-    ];
+    options = [ "noatime" ];
   };
+
+  systemd.tmpfiles.rules = [
+    "d ${forgejo.storageDir}/backups 0755 forgejo forgejo -"
+    "d ${forgejo.storageDir}/lfs 0755 forgejo forgejo -"
+    "d ${forgejo.storageDir}/repositories 0755 forgejo forgejo -"
+  ];
 }
