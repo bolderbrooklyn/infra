a6012992306a2624e061d8d3c8252bf821c33667
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-05 11:37:03 -0800

    remove k3s postgres auth

--- nix/modules/postgresql/default.nix
+++ nix/modules/postgresql/default.nix
@@ -4,10 +4,6 @@
     enable = true;
     package = pkgs.postgresql_18;
     enableTCPIP = true;
-
-    authentication = ''
-      host all all 10.42.0.0/24 scram-sha-256
-    '';
   };
 
   networking.firewall.allowedTCPPorts = [
