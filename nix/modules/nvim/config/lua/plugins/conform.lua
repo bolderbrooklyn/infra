5d42fcb45a8a30102e1688ec5e391c550ffb4d9c
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-07 15:47:58 -0800

    add biome-organize-imports to conform

---
+++ nix/modules/nvim/config/lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+local biome_supported = {
+	"astro",
+	"css",
+	"graphql",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"jsonc",
+	"svelte",
+	"typescript",
+	"typescriptreact",
+	"vue",
+}
+
+return {
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = function(_, opts)
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			for _, ft in ipairs(biome_supported) do
+				opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+				table.insert(opts.formatters_by_ft[ft], "biome")
+				table.insert(opts.formatters_by_ft[ft], "biome-organize-imports")
+			end
+
+			opts.formatters = opts.formatters or {}
+			opts.formatters.biome = {
+				require_cwd = true,
+			}
+		end,
+	},
+}
