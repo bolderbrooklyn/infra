18c17d9f1b45ce44bbc37eca9ca65a777e30da11
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-06 21:28:17 -0800

    set default neotest overseer component aliases

--- nix/modules/nvim/config/lua/plugins/overseer.lua
+++ nix/modules/nvim/config/lua/plugins/overseer.lua
@@ -1,15 +1,20 @@
 return {
 	{
 		"stevearc/overseer.nvim",
-		opts = {
-			component_aliases = {
-				default = {
-					"on_exit_set_status",
-					"on_complete_notify",
-					{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
-					{ "open_output", on_start = "always", direction = "dock", focus = false },
-				},
-			},
-		},
+		opts = function(_, opts)
+			opts = opts or {}
+			opts.component_aliases = opts.component_aliases or {}
+			opts.component_aliases.default = {
+				"on_exit_set_status",
+				"on_complete_notify",
+				{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+				{ "open_output", on_start = "if_no_on_output_quickfix", direction = "dock", focus = false },
+			}
+			opts.component_aliases.default_neotest = {
+				"on_exit_set_status",
+				"on_complete_notify",
+				{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+			}
+		end,
 	},
 }
