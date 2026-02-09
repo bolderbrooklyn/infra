eaad5dba4d6bf6573d0faee2cf84fb92cb28c616
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-09 14:53:35 -0800

    add treesitter-context to lazyvim

--- nix/modules/nvim/config/lua/config/lazy.lua
+++ nix/modules/nvim/config/lua/config/lazy.lua
@@ -53,6 +53,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.ui.indent-blankline" },
 		{ import = "lazyvim.plugins.extras.ui.smear-cursor" },
+		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
 		{ import = "lazyvim.plugins.extras.util.dot" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		{ import = "lazyvim.plugins.extras.util.octo" },
