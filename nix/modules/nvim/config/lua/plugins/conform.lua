c37fa31a99ed160816458007d1da6a14874913d0
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-10 09:59:25 -0800

    add standardrb to conform.nvim
    conditionally use rubocop or standardrb for formatting depending on
    presence of config files

--- nix/modules/nvim/config/lua/plugins/conform.lua
+++ nix/modules/nvim/config/lua/plugins/conform.lua
@@ -17,6 +17,9 @@ return {
 	optional = true,
 	opts = function(_, opts)
 		opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+		opts.formatters_by_ft.ruby = { "standardrb", "rubocop" }
+
 		for _, ft in ipairs(biome_supported) do
 			opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
 			table.insert(opts.formatters_by_ft[ft], "biome")
@@ -24,8 +27,21 @@ return {
 		end
 
 		opts.formatters = opts.formatters or {}
+
 		opts.formatters.biome = {
 			require_cwd = true,
+		}
+
+		opts.formatters.standardrb = {
+			condition = function(_, ctx)
+				return vim.fs.find({ ".standard.yml" }, { path = ctx.filename, upward = true })[1]
+			end,
+		}
+
+		opts.formatters.rubocop = {
+			condition = function(_, ctx)
+				return vim.fs.find({ ".rubocop.yml" }, { path = ctx.filename, upward = true })[1]
+			end,
 		}
 	end,
 }
