919c7fa0800d85c765b7b1a247ca3827e4b5fbc6
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-09 16:05:11 -0800

    add octo keymaps

---
+++ nix/modules/nvim/config/lua/plugins/octo.lua
@@ -0,0 +1,12 @@
+return {
+	"pwntester/octo.nvim",
+	keys = {
+		{ "<leader>goi", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
+		{ "<leader>goI", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
+		{ "<leader>gop", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+		{ "<leader>goP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
+		{ "<leader>gor", "<cmd>Octo repo view<CR>", desc = "Show Repo (Octo)" },
+		{ "<leader>goR", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
+		{ "<leader>goS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+	},
+}
