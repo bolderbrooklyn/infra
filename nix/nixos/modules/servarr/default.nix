00d1d7d15df1aa418f6aff03a69c69affc7b0ebb
jesse brooklyn hannah <jesse@jbhannah.net>
2026-02-19 22:23:38 -0800

    remove transmission incomplete directory setting

--- nix/nixos/modules/servarr/default.nix
+++ nix/nixos/modules/servarr/default.nix
@@ -45,7 +45,6 @@ in
         blocklist-enabled = true;
         blocklist-url = "https://raw.githubusercontent.com/Naunter/BT_BlockLists/master/bt_blocklists.gz";
         download-dir = "/mnt/genesect/media/Downloads";
-        incomplete-dir = "/mnt/genesect/media/Downloads/.incomplete";
         rpc-bind-address = "0.0.0.0";
         rpc-host-whitelist-enabled = false;
         rpc-whitelist-enabled = false;
