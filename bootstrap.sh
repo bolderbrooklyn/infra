6976c0c886d29d4546a0dc5a8f1dffb9a15f7cf8
brooke hannah <bhannah@nclusion.com>
2026-02-23 12:08:42 -0800

    fix bootstrap script
    - don't fail if xcode cli tools are already installed
    - source nix command before darwin-rebuild

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 
 if [[ "$(uname)" == "Darwin" ]]; then
   # install xcode command line tools
-  xcode-select --install
+  xcode-select --install || echo "Xcode command line tools are already installed"
 
   # install lix
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install
 
   # install nix-darwin and apply the flake
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
   sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#"$(hostname -s)"
 fi
