3f7b6e12e1544b3c76a602648ff56b10822aea45
brooke hannah <bhannah@nclusion.com>
2026-02-23 13:59:44 -0800

    set more macos defaults

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -27,6 +27,16 @@
     };
 
     targets.darwin.defaults = {
+      "com.apple.desktopservices" = {
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+
+      "com.apple.dock" = {
+        autohide = true;
+        size-immutable = true;
+      };
+
       "com.apple.finder" = {
         _FXSortFoldersFirst = true;
         _FXSortFoldersFirstOnDesktop = true;
@@ -35,6 +45,7 @@
 
       "com.apple.menuextra.clock" = {
         Show24Hour = true;
+        ShowAMPM = false;
         ShowDate = 0;
       };
 
@@ -47,8 +58,10 @@
       };
 
       NSGlobalDomain = {
+        AppleICUForce24HourTime = true;
         AppleShowAllExtensions = true;
         AppleShowAllFiles = true;
+        AppleShowScrollBars = "WhenScrolling";
         InitialKeyRepeat = 25;
         KeyRepeat = 2;
       };
