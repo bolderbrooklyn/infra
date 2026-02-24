0de4aa54889845b223a8022cee00da1ce6c78181
brooke hannah <bhannah@nclusion.com>
2026-02-23 16:58:41 -0800

    make bootstrap script curlable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+WORKDIR="$HOME/Developer/Repositories/codeberg.org/bolderbrook/infra"
+
 if [[ "$(uname)" == "Darwin" ]]; then
   # install xcode command line tools
   xcode-select --install || echo "Xcode command line tools are already installed"
+fi
 
+git clone https://codeberg.org/bolderbrook/infra.git "$WORKDIR"
+cd "$WORKDIR"
+
+if [[ "$(uname)" == "Darwin" ]]; then
   # install lix
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install
 
