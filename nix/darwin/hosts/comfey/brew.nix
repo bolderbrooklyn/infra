ce31124a3b830b54c6be207e224ea664fc015322
brooke hannah <bhannah@nclusion.com>
2026-02-25 09:53:59 -0800

    add more casks to comfey

--- nix/darwin/hosts/comfey/brew.nix
+++ nix/darwin/hosts/comfey/brew.nix
@@ -2,11 +2,20 @@
   homebrew = {
     casks = [
       "chatgpt"
+      "dash"
+      {
+        name = "google-chrome";
+        args.appdir = "/Applications";
+      }
       "linear-linear"
       "logi-options+"
       "notion"
       "notion-calendar"
       "notion-mail"
+      {
+        name = "slack";
+        args.appdir = "/Applications";
+      }
     ];
   };
 }
