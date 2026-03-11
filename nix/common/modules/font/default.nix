349667493c58cc3182e90b866751d867758cb6cb
brooke hannah <bhannah@nclusion.com>
2026-03-11 14:46:34 -0700

    add with lib to font module

--- nix/common/modules/font/default.nix
+++ nix/common/modules/font/default.nix
@@ -4,20 +4,21 @@
   pkgs,
   ...
 }:
+with lib;
 {
   options.gui.font = {
-    name = lib.mkOption {
-      type = lib.types.str;
+    name = mkOption {
+      type = types.str;
       default = "CaskaydiaCove Nerd Font";
     };
 
-    package = lib.mkOption {
-      type = lib.types.package;
+    package = mkOption {
+      type = types.package;
       default = pkgs.nerd-fonts.caskaydia-cove;
     };
 
-    size = lib.mkOption {
-      type = lib.types.int;
+    size = mkOption {
+      type = types.int;
       default = 15;
     };
   };
