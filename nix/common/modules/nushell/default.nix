6cb2654f21f4590c3bc97317c94b1f3bd5523d59
brooke hannah <bhannah@nclusion.com>
2026-03-11 14:48:50 -0700

    simplify nushell enable option usage

--- nix/common/modules/nushell/default.nix
+++ nix/common/modules/nushell/default.nix
@@ -9,7 +9,7 @@ let
   inherit (config.common) username;
   inherit (config.home-manager.users.${username}.programs.nushell) enable;
 in
-{
-  environment.systemPackages = lib.mkIf enable [ nushell ];
-  environment.shells = lib.mkIf enable [ nushell ];
+lib.mkIf enable {
+  environment.systemPackages = [ nushell ];
+  environment.shells = [ nushell ];
 }
