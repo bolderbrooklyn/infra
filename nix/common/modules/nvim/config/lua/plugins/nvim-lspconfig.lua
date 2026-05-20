e741618364716e1ff8381b0fa56452f6bc9dcbee
jesse brooklyn hannah <jesse@jbhannah.net>
2026-05-20 16:57:53 -0700

    force disable nil ls

--- nix/common/modules/nvim/config/lua/plugins/nvim-lspconfig.lua
+++ nix/common/modules/nvim/config/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	opts = {
 		servers = {
+			nil_ls = { enabled = false },
 			nixd = { enabled = true },
 			pyright = { enabled = false },
 			standardrb = { enabled = true },
