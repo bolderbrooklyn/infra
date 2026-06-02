4ce93a76672fed677685884679643be057e4bc1c
jesse brooklyn hannah <jesse@jbhannah.net>
2026-06-01 23:40:41 -0700

    more utils in kalmiya

--- nix/kalmiya-home/home.nix
+++ nix/kalmiya-home/home.nix
@@ -1,13 +1,10 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 let
   user = "kalmiya";
   homeDir = "/home/${user}";
-  npmPrefix = "${homeDir}/.npm-global";
 in
 {
   age.secrets.openclaw-env = {
@@ -18,48 +15,37 @@ in
     username = user;
     homeDirectory = homeDir;
 
-    sessionPath = [
-      "${homeDir}/.local/bin"
-      "${npmPrefix}/bin"
-    ];
-
     packages = with pkgs; [
       _1password-cli
       fd
       ffmpeg-headless
-      mcporter
+      gcc
+      gnumake
+      opus
+      python311
       sqlite-vec
+      uv
+      yq
     ];
 
     stateVersion = "26.05";
     enableNixpkgsReleaseCheck = false;
   };
 
-  home.activation.createOpenclawEnv = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "${config.home.homeDirectory}/.openclaw"
-    ln -sf "${config.age.secrets.openclaw-env.path}" "${config.home.homeDirectory}/.openclaw/.env"
-  '';
-
   programs = {
     home-manager.enable = true;
 
     bash.enable = true;
-
     chromium.enable = true;
     git.enable = true;
     jq.enable = true;
-    neovim.enable = true;
+    npm.enable = true;
     ripgrep.enable = true;
     tmux.enable = true;
-
-    npm = {
-      enable = true;
-
-      settings = {
-        prefix = npmPrefix;
-      };
-    };
   };
 
-  xdg.enable = true;
+  xdg = {
+    enable = true;
+    localBinInPath = true;
+  };
 }
