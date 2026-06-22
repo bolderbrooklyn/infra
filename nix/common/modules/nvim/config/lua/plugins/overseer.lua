04f37b434f5cdf25aebf9aa2cec27279d4341099
jesse brooklyn hannah <jesse@jbhannah.net>
2026-06-22 10:59:05 -0700

    enable overseer task caching

--- nix/common/modules/nvim/config/lua/plugins/overseer.lua
+++ nix/common/modules/nvim/config/lua/plugins/overseer.lua
@@ -14,5 +14,15 @@ return {
 				{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
 			},
 		},
+		template_cache_threshold_ms = 200,
 	},
+	init = function()
+		local overseer = require("overseer")
+		vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+			callback = function()
+				local cwd = vim.v.cwd or vim.fn.getcwd()
+				overseer.preload_task_cache({ dir = cwd })
+			end,
+		})
+	end,
 }
