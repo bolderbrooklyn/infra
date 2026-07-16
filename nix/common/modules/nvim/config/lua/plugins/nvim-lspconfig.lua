1022dbee0165190396f5f842980996424ca85f25
jesse brooklyn hannah <jesse@jbhannah.net>
2026-07-16 13:47:12 -0700

    remove standardrb from nvim lspconfig

--- nix/common/modules/nvim/config/lua/plugins/nvim-lspconfig.lua
+++ nix/common/modules/nvim/config/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,6 @@ return {
 			nil_ls = { enabled = false },
 			nixd = { enabled = true },
 			pyright = { enabled = false },
-			standardrb = { enabled = true },
 			tsgo = { enabled = true },
 			ty = { enabled = true },
 			vtsls = { enabled = false },
