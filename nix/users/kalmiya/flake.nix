0ddb353bf369b446f839cba134eb1c4e55a1fd2b
jesse brooklyn hannah <jesse@jbhannah.net>
2026-07-24 09:32:40 -0700

    install hermes agent for kalmiya

--- nix/users/kalmiya/flake.nix
+++ nix/users/kalmiya/flake.nix
@@ -8,13 +8,28 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    llm-agents = {
+      url = "github:numtide/llm-agents.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      llm-agents,
+      ...
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.appendOverlays [
+        llm-agents.overlays.shared-nixpkgs
+      ];
+    in
     {
       homeConfigurations.kalmiya = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        inherit pkgs;
 
         modules = [ ./home.nix ];
       };
--- nix/users/kalmiya/home.nix
+++ nix/users/kalmiya/home.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home = {
     username = "kalmiya";
     homeDirectory = "/home/kalmiya";
+
+    packages = [ pkgs.llm-agents.hermes-agent ];
+
     stateVersion = "26.05";
   };
 }
