d5a386f770bb73e03f963f81f7cad49b8e4fc0d0
jesse brooklyn hannah <jesse@jbhannah.net>
2026-07-24 13:58:48 -0700

    add xdg bin to path

--- nix/system-manager/hosts/kalmiya/users/kalmiya/home.nix
+++ nix/system-manager/hosts/kalmiya/users/kalmiya/home.nix
@@ -1,21 +1,28 @@
 { pkgs, ... }:
 {
-  home-manager.users.kalmiya.home = {
-    packages = with pkgs; [
-      _1password-cli
-      fd
-      ffmpeg
-      git
-      gnumake
-      jq
-      libffi
-      llvm
-      nodejs
-      python3
-      ripgrep
-      uv
-    ];
+  home-manager.users.kalmiya = {
+    home = {
+      packages = with pkgs; [
+        _1password-cli
+        fd
+        ffmpeg
+        git
+        gnumake
+        jq
+        libffi
+        llvm
+        nodejs
+        python3
+        ripgrep
+        uv
+      ];
 
-    stateVersion = "26.05";
+      stateVersion = "26.05";
+    };
+
+    xdg = {
+      enable = true;
+      localBinInPath = true;
+    };
   };
 }
